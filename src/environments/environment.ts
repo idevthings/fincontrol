8e4142169a628f74b7ce8d7e7dfc54f03ad32870
Miguel Pérez Palacios <idevthings101@gmail.com>
2025-09-07 13:52:56 +0200

    use keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: process.env['SUPABASE_URL'] || '',
-  supabaseKey: process.env['SUPABASE_ANON_KEY'] || ''
+  supabaseUrl: 'https://nsqswyxsduopbcvtylgb.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zcXN3eXhzZHVvcGJjdnR5bGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwOTI1MTQsImV4cCI6MjA3MjY2ODUxNH0.jlSIA9ANPyLEY7MP8A_2CvpFKJmju-epNQy38OGMJQc'
 };
