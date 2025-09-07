d886c41ba8bebe608fdf806b2ef9940e61b57e72
Miguel Pérez Palacios <idevthings101@gmail.com>
2025-09-07 13:42:12 +0200

    ui fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,30 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('fincontrol');
+  protected readonly isImportMode = signal(false);
+
+  constructor(private router: Router) {}
+
+  toggleMode() {
+    const newMode = !this.isImportMode();
+    this.isImportMode.set(newMode);
+    
+    // Navigate to the appropriate route
+    if (newMode) {
+      this.router.navigate(['/import']);
+    } else {
+      this.router.navigate(['/categorize']);
+    }
+  }
 }
